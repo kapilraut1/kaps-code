1eabf7f0b0eb4fabc6cb95cdce7e3ff25683ae9e
Kapil <kapilraut026@gmail.com>
2025-09-07 23:17:54 +0545

    code-pen

---
+++ code-pen/src/Editor.jsx
@@ -0,0 +1,43 @@
+import React, {useState} from "react";
+import CodeMirror from "@uiw/react-codemirror";
+import { oneDark } from "@codemirror/theme-one-dark";
+import { html } from "@codemirror/lang-html";
+import { css } from "@codemirror/lang-css";
+import { javascript } from "@codemirror/lang-javascript";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faCompressAlt, faExpandAlt} from '@fortawesome/free-solid-svg-icons'
+
+const Editor = ({ language, displayName, value, onChange }) => {
+  const getExtension = () => {
+    switch (language) {
+      case "xml":
+        return html();
+      case "css":
+        return css();
+      case "javascript":
+        return javascript();
+      default:
+        return [];
+    }
+  };
+
+  const [open, setOpen] = useState(true);
+
+  return (
+    <div className={`editor-container ${open? '':'collapsed' }`}>
+      <div className="editor-title">
+        {displayName}
+        <button onClick={()=> setOpen(prevOpen => !prevOpen)}><FontAwesomeIcon icon={open? faCompressAlt: faExpandAlt} /></button>
+      </div>
+      <CodeMirror
+        value={value}
+        height="600px"
+        theme={oneDark}
+        extensions={[getExtension()]}
+        onChange={(value) => onChange(value)}
+      />
+    </div>
+  );
+};
+
+export default Editor;
